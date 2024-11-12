893e92d8f9746e4301e24376b2884c4a86b9fec3
CPPavithra <cppavithra05@gmail.com>
2024-11-12 18:24:33 +0530

    included header for reverse and syntax error correction

--- astar.cpp
+++ astar.cpp
@@ -43,25 +43,25 @@ double heuristic(int x1, int y1, int x2, int y2)
 {
 	return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 }
-vector<Node>astar(Node start, Node goal)
+vector<Node>astar(const vector<vector<int>>& grid, Node start, Node goal)
 {
    priority_queue<Node*,vector<Node*>,comparenode>openlist;//priority list for 
    unordered_map<int, Node*>allNodes;//map for all the nodes
    start.h_cost = heuristic(start.x, start.y, goal.x, goal.y);//euclidean distance function h cost
    start.f_cost = start.g_cost + start.h_cost;
-   openList.push(&start);
+   openlist.push(&start);
 
    while(!openlist.empty())
    {
      //stack
-     Node*current=openList.top();
-     openList.pop();//read one by one
+     Node*current=openlist.top();
+     openlist.pop();//read one by one
      if(current->x==goal.x && current->y==goal.y)
      {
 	     vector <Node> path;
 	     for(Node *n=current;n!=NULL;n=n->parent)
 	     {
-		     path.push_back(n);
+		     path.push_back(*n);//error
 	     }
 	     reverse(path.begin(), path.end()); //reverse path
 	     return path;
@@ -86,7 +86,7 @@ vector<Node>astar(Node start, Node goal)
 	     }
 	     Node* neighbour = new Node(newx, newy, newg_cost, heuristic(newx, newy, goal.x, goal.y), current);//create a new neighbour node
              //newx+newy shows collision
-	     int key=newx*grid[0].size+newy;
+	     int key=newx*grid[0].size()+newy;
 	     
 	     if(allNodes.find(key)==allNodes.end()||neighbour->g_cost<allNodes.find(key)->g_cost)//newx+newy is the unique key for map
 	     {
@@ -105,15 +105,15 @@ int main(){
 //SAMPLE GRID
 vector<vector<int>> grid = {
   {0, 0, 0, 0, 0},
-  {0, 1, 1, 1, 0},
-  {0, 0, 0, 0, 0},
+  {0, 1, 1, 1, 1},
+  {1, 0, 0, 0, 0},
   {0, 1, 1, 1, 0},
   {0, 0, 0, 0, 0}
 };
 
 Node start(0, 0);
 Node goal(4, 4);
-vector<Node>path = astar(grid,start,goal)
+vector<Node>path = astar(grid,start,goal);
 if(path.empty())
 {
 	cout<<"No path found"<<endl;
