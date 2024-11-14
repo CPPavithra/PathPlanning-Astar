a60ff8992d5833c6206708790ab5038ec2703308
CPPavithra <cppavithra05@gmail.com>
2024-11-14 07:26:22 +0530

    changed it back to 1x1 coordinates

--- astar.cpp
+++ astar.cpp
@@ -39,14 +39,14 @@ struct comparenode {
     }
 };
 
-double heuristic(float x1, float y1, float x2, float y2)
+double heuristic(int x1, int y1, int x2, int y2)
 {
 	return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 }
-vector<Node>astar(const vector<vector<float>>& grid, Node start, Node goal,float squaresize)
+vector<Node>astar(const vector<vector<int>>& grid, Node start, Node goal)
 {
    priority_queue<Node*,vector<Node*>,comparenode>openlist;//priority list for 
-   unordered_map<float, Node*>allNodes;//map for all the nodes
+   unordered_map<int, Node*>allNodes;//map for all the nodes
    start.h_cost = heuristic(start.x, start.y, goal.x, goal.y);//euclidean distance function h cost
    start.f_cost = start.g_cost + start.h_cost;
    openlist.push(&start);
@@ -66,37 +66,35 @@ vector<Node>astar(const vector<vector<float>>& grid, Node start, Node goal,float
 	     reverse(path.begin(), path.end()); //reverse path
 	     return path;
      }
-   float dx[] = {squaresize, -(squaresize), 0, 0, squaresize, -(squaresize), squaresize, -(squaresize)};
-   float dy[] = {0, 0, squaresize, -(squaresize), squaresize, -(squaresize), -(squaresize), squaresize}; //for variable square grid size
-										  /*float dx[] = {1,-1,0,0,1,-1,1,-1);
-    float dy[] = {0,0,1,-1,1,-1,-1,1}; for 1x1 grid square size*/
+    //for variable square grid size
+    int dx[] = {1,-1,0,0,1,-1,1,-1};
+    int dy[] = {0,0,1,-1,1,-1,-1,1}; 
 
    int i; double newg_cost;
    for(i=0;i<8;i++)
    {
-	   float newx=current->x+dx[i];
-	   float newy=current->y+dy[i];
-
-     if(newx>=0 && newx<grid.size()*squaresize && grid[newx][newy]==0 && newy>=0 && newy<grid[0].size()*squaresize)//boundary condition checking
+	   int newx=current->x+dx[i];
+	   int newy=current->y+dy[i];
+     if(newx>=0 && newx<grid.size() && grid[newx][newy]==0 && newy>=0 && newy<grid[0].size())//boundary condition checking
      {
 	     if(i<4)//frist 4 in the array is for straight movements
 	     {
-		     newg_cost=current->g_cost+1.0*(squaresize);
+		     newg_cost=current->g_cost+1.0;
 	     }
 	     else
 	     {
-                    newg_cost=current->g_cost+1.414*(squaresize);
+                    newg_cost=current->g_cost+1.414;
 	     }
 	     Node* neighbour = new Node(newx, newy, newg_cost, heuristic(newx, newy, goal.x, goal.y), current);//create a new neighbour node
              //newx+newy shows collision
-	     float key=newx*grid[0].size()*squaresize+newy;
+	     int key=newx*grid[0].size()+newy;
 	     
 	     if(allNodes.find(key)==allNodes.end() || neighbour -> g_cost<allNodes.find(key) -> second -> g_cost)//newx+newy is the unique key for map
 	     {
                neighbour->f_cost=neighbour->g_cost+neighbour->h_cost;
 	       openlist.push(neighbour);
 	       //update all nodes
-	       allNodes[newx+newy]=neighbour;
+	       allNodes[key]=neighbour;
 	     }
 
      } 
@@ -105,28 +103,18 @@ vector<Node>astar(const vector<vector<float>>& grid, Node start, Node goal,float
 return{}; //return empty if not found in while loop
 }
 int main(){
-float squaresize;
-cout<<"Enter the single square size for the grid: ";
-cin>>squaresize;
-/*SAMPLE GRID
-vector<vector<float>> grid = {
-  {0, 0, 0, 0, 0},
+//SAMPLE GRID
+vector<vector<int>> grid = {
+ /*(0,0)*/ {0, 0, 0, 0, 0},
   {0, 1, 1, 1, 1},
   {1, 0, 0, 0, 0},
   {0, 1, 1, 1, 0},
-  {0, 0, 0, 0, 0}
-};*/
-vector<vector<float>> grid = {
-  {0, 0, 0, 0, 0},
-  {0, 1, 1, 1, 1},
-  {1, 0, 0, 0, 0},
-  {0, 1, 1, 1, 0},
-  {0, 0, 0, 0, 0}
+  {0, 0, 0, 0, 0}/*(4,4)*/
 };
 
 Node start(0, 0);
 Node goal(4, 4);
-vector<Node>path = astar(grid,start,goal,squaresize);
+vector<Node>path = astar(grid,start,goal);
 if(path.empty())
 {
 	cout<<"No path found"<<endl;
