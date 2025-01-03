98809c2d3c1f4e33a39527f2b5d563538abdc6fb
CPPavithra <cppavithra05@gmail.com>
2025-01-03 11:19:53 +0530

    main file to combine logic

--- main.cpp
+++ main.cpp
@@ -1,97 +1,188 @@
-#include <iostream>
-#include <vector>
-#include <fstream>
-#include <sstream>
-#include <stdexcept>
-#include "include/gridmap.h" 
-#include "include/astar.h"
-
-using namespace std;
-void save_gridmap_to_csv(const vector<vector<int>>& grid, const string& filename) {
-    ofstream file(filename);
-    if (!file.is_open()) {
-        cerr<<"Could not open"<<endl;
-        exit(EXIT_FAILURE);
-    }
-    for (const auto& row:grid) {
-        for (size_t i=0; i<row.size(); ++i) {
-            file << row[i];
-            if (i<row.size()-1) file<<",";
-        }
-        file<<"\n";
-    }
-    file.close();
-}
-
-vector<vector<int>> readGridFromCSV(const string& filename) {
-    vector<vector<int>> grid;
-    ifstream file(filename);
-    if (!file.is_open()) {
-        throw runtime_error("Could not open CSV file.");
-    }
-    string line;
-    while (getline(file, line)) {
-        stringstream ss(line);
-        string value;
-        vector<int> row;
-        while (getline(ss, value, ',')) {
-            row.push_back(stoi(value));
-        }
-        grid.push_back(row);
-    }
-    file.close();
-    return grid;
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include "rerun.h"
+#include "astar.h"
 
 int main()
 {
-    string ply_file="pointcloud.ply";
-    float grid_resolution=0.1;
-    //to form the grid map
-    Gridmap grid_map=create_gridmap(ply_file,grid_resolution);
+        auto rec = rerun::RecordingStream("gridmap");
+        rec.spawn().exit_on_failure();
 
-    //converting boolean to anint grid
-    vector<vector<int>>grid;
-    for (const auto& row:grid_map.occupancy_grid)
-    {
-        vector<int> int_row(row.begin(),row.end());
-        grid.push_back(int_row);
-    }
+        //realsense pipeline
+        rs2::pipeline pipe;
+        rs2::config cfg;
+        cfg.enable_device_from_file("video2.bag");
+        cfg.enable_stream(RS2_STREAM_GYRO);
+        cfg.enable_stream(RS2_STREAM_ACCEL);
+        cfg.enable_stream(RS2_STREAM_DEPTH);
 
-    //saving it to a csv file
-    string csv_file = "gridmap2.csv";
-    save_gridmap_to_csv(grid,csv_file);
-    try
-    {
-        grid=readGridFromCSV(csv_file);
-    }
-    catch (const exception& e)
-    {
-        cerr<<"Error:"<<e.what()<<endl;
-        return 1;
-    }
-    cout << "Grid map loaded successfully." << endl;
-    double min_x=0;
-    double max_y=grid[0].size()-1;
-    double min_y=0;
-    double max_y=grid.size()-1;
-    //start and end
-    Node start(0,0);
-    //Node goal(grid.size() - 1, grid[0].size() - 1);
-    Node goal(5,6);
+       try {
+                pipe.start(cfg);
+       } catch (const rs2::error &e) {
+                cerr << "Error: Failed to start the pipeline: " << e.what() << endl;
+                return -1;
+      }
+      rs2::pointcloud pc;
+      rs2::points points;
 
-    //a star
-    vector<Node> path = astar(grid, start, goal);
-   
-    if (path.empty()) {
-        cout<<"No path"<<endl;
-    } else {
-        cout<<"Path found:"<<endl;
-        for (const auto& node:path) {
-            cout<<"("<<node.x<<", "<<node.y<<")"<<endl;
+        //for rover pose
+      Pose rover_pose;
+      rover_pose.position = Eigen::Vector3f(0, 0, 0);
+      rover_pose.orientation = Eigen::Matrix3f::Identity();
+      rover_pose.velocity = Eigen::Vector3f(0, 0, 0);
+
+        //for time
+     auto last_time = std::chrono::high_resolution_clock::now();
+
+     vector<Vector3f> point_vectors;
+
+     static int frame_counter = 0;
+          while (true){
+        //get the current time and compute the delta time
+        auto current_time = std::chrono::high_resolution_clock::now();
+        float delta_time = std::chrono::duration<float>(current_time - last_time).count();
+        last_time = current_time;
+       //declare variables to hold sensor data
+        rs2_vector accel_data = {0.0f, 0.0f, 0.0f};
+        rs2_vector gyro_data = {0.0f, 0.0f, 0.0f};
+
+        //get frames from the RealSense camera
+        rs2::frameset frameset;
+        try {
+                frameset = pipe.wait_for_frames();
+        } catch (const rs2::error& e) {
+                std::cerr << "RealSense error: " << e.what() << std::endl;
+                continue;
         }
+
+
+        //get accelerometer data
+        if (rs2::motion_frame accel_frame = frameset.first_or_default(RS2_STREAM_ACCEL))
+        {
+                accel_data = accel_frame.get_motion_data();
+        }
+        else
+        {
+                cerr<< "Failed to retrieve accelerometer data" << endl;
+        }
+        //get gyroscope data
+        if (rs2::motion_frame gyro_frame = frameset.first_or_default(RS2_STREAM_GYRO))
+        {
+                gyro_data = gyro_frame.get_motion_data();
+        }
+        else
+        {
+                cerr<< "Failed to retrieve gyroscope data"<< endl;
+        }
+                //convert accelerometer and gyroscope data to Eigen vectors
+        Eigen::Vector3f accel_eigen = convert_to_eigen_vector(accel_data);
+        Eigen::Vector3f gyro_eigen = convert_to_eigen_vector(gyro_data);
+
+        //update the rover pose with accelerometer and gyroscope data
+        update_rover_pose(rover_pose, accel_eigen, gyro_eigen, delta_time);
+
+        //process depth data to create point cloud
+        rs2::depth_frame depth_frame = frameset.get_depth_frame();
+                points = pc.calculate(depth_frame);
+
+                //collect point cloud data
+                point_vectors.clear();
+                for (size_t i=0; i<points.size(); ++i) {
+                auto point=points.get_vertices()[i];
+                if (point.z) {
+                        Eigen::Vector3f transformed_point=rover_pose.orientation*Eigen::Vector3f(point.x, point.y, point.z)+rover_pose.position;
+                        point_vectors.push_back(transformed_point);
+                }
+ }
+                //convert the realsense points to pcl point
+                auto pcl_cloud=convert_to_pcl(point_vectors);
+
+  if (depth_frame) {
+    static int processed_frames = 0;  // Tracks how many frames you process
+    processed_frames++;
+    auto frame_number = depth_frame.get_frame_number();
+
+    std::cout << "Iteration: " << processed_frames
+              << ", Frame Number: " << frame_number << std::endl;
+}
+
+    //passthrough filter
+    pcl::PointCloud<pcl::PointXYZ>::Ptr passthrough_cloud(new pcl::PointCloud<pcl::PointXYZ>());
+    pcl::PassThrough<pcl::PointXYZ> passthrough;
+    passthrough.setInputCloud(pcl_cloud);
+    passthrough.setFilterFieldName("z");  //this is based on depth
+    passthrough.setFilterLimits(0.5, 5.0); //range in metres.
+    passthrough.filter(*passthrough_cloud);
+    //cout<<"After filtering AFTER PASSTHROUGH: "<<passthrough_cloud->size()<<" points."<<"\n"<<endl;
+
+    //voxelgrid
+    pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud(new pcl::PointCloud<pcl::PointXYZ>());
+    pcl::VoxelGrid<pcl::PointXYZ> voxel;
+    voxel.setInputCloud(passthrough_cloud);
+    voxel.setLeafSize(0.05f, 0.05f, 0.05f); //this is the size of each voxel in xyz dimension
+    voxel.filter(*filtered_cloud);
+
+    point_vectors.clear();
+    for (const auto& point : filtered_cloud->points) {
+        point_vectors.emplace_back(point.x, point.y, point.z);
     }
 
-    return 0;
+
+    create_gridmap(gridmap, point_vectors, rover_pose, grid_resolution);
+
+   /* cout<<"Generated PointCloud: "<< points.size()<< " points."<<"\n"<<endl;
+    cout<<"After filtering FULLY: "<<filtered_cloud->size()<<" points."<<"\n"<<endl;*/
+    //to draw gridmap only after sometime
+
+     if(gridmap.occupancy_grid.size()>=batch_threshold)
+     {
+        draw_gridmap(gridmap,point_vectors, rover_pose, grid_resolution, rec);
+        batch_threshold=batch_threshold+gridmap.occupancy_grid.size();
+     }  //std::this_thread::sleep_for(std::chrono::milliseconds(30));
+    if(gridmap.occupancy_grid.size()>=batch_threshold)
+     {
+        draw_gridmap(gridmap,point_vectors, rover_pose, grid_resolution, rec);
+        batch_threshold=batch_threshold+gridmap.occupancy_grid.size();
+        cout<<"Setting boundaries"<<endl;
+        int startx,starty,goalx,goaly;
+        cout<<"Enter start: (";
+        cin>>startx;
+        cout<<" , ";
+        cin>>starty;
+        cout<<")"<<"\n"<<endl;
+        cout<<"Enter goal: (";
+        cin>>goalx;
+        cout<<" , ";
+        cin>>goaly;
+        cout<<")"<<"\n"<<endl;
+        if(startx<min_x || starty<min_y || goalx>max_x || goaly>max_y)
+        {
+                cout<<"Out of bound query"<<endl;
+        }else
+        {Node start(startx,starty);
+        Node goal(goalx,goaly);
+
+        cout<<"Start and goal node set astar starts"<<endl;
+
+        vector<Node>path = astar(grid,start,goal);
+if(path.empty())
+{
+        cout<<"No path found"<<endl;
+}
+else
+{
+        cout<<"Path found"<<endl;
+        for(const Node&node:path)
+        {
+                cout<<"("<<node.x<<","<<node.y<<")";
+        }
+        cout<<endl;
+}
+
+        }
+
+}
+        return 0;
+
 }
 
