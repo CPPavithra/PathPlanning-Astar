5a62dd102ab5e89e79ab4f5fa6529fe460e95155
CPPavithra <cppavithra05@gmail.com>
2025-01-10 00:37:59 +0530

    path planning logic wrong- check

--- astar.cpp
+++ astar.cpp
@@ -32,38 +32,6 @@
 
 using namespace std;
 
-/*struct Node
-{
-	float x,y;
-	double g_cost,h_cost,f_cost;
-	Node *parent;
-	//using an initialiser list instead of this keyword
-	Node(float x,float y, double g_cost=0,double h_cost=0,Node *parent=NULL): x(x), y(y), g_cost(g_cost), h_cost(h_cost), f_cost(g_cost+h_cost), parent(parent) {}
-        bool operator>(const Node& other) const
-       	{ 
-		return f_cost > other.f_cost;
-       	}//overloading > to comapre between final cost of the nodes
-
-       /* Node(float x, int y, double g_cost=0, double h_cost=0, Node* parent=NULL) {
-            this->x = x;            
-            this->y = y;           
-            this->g_cost = g_cost;            
-            this->h_cost = h_cost;            
-            this->f_cost = g_cost + h_cost;        
-            this->parent = parent;  
-          }
-
-        bool operator>(const Node& other) const {
-            return f > other.f;
-         }*/
-//};
-
-/*struct comparenode {
-    bool operator()(Node*a, Node*b)
-    {
-            return *a>*b;
-    }
-};*/
 
 double heuristic(int x1, int y1, int x2, int y2)
 {
